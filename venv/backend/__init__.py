f5425401a653086f14e9bfea5414f55e6c198f4e
zaramichaela <zarateo@gmail.com>
2020-01-08 11:22:34 +0800

    added sheng de stuff (shele dmb error)

--- venv/backend/__init__.py
+++ venv/backend/__init__.py
@@ -23,13 +23,37 @@ configure_uploads(app, (images,))
 def login():
     return render_template('login.html')
 
-
-
 @app.route('/register')
 def about():
     return render_template('register.html')
 
+@app.route('/cart')
+def cart():
+    usersDict = {}
+    db = shelve.open('storage.db', 'r')
+    usersDict = db['ShoppingCart']
+    db.close()
 
+    usersList = []
+   #create user object 1, store in variable u1
+    u1 = ShoppingCart.ShoppingCart("Medicine1", "D11", 4.00, 1)
+    usersList.append(u1)
+   #create user object 2, store in variable u2
+    u2 = ShoppingCart.ShoppingCart("Medicine2", "D12", 2.00, 3)
+    usersList.append(u2)
+    u3 = ShoppingCart.ShoppingCart("Medicine3", "D13", 1.00, 2)
+    usersList.append(u3)
+
+    for key in usersDict:
+        user = usersDict.get(key)
+        usersList.append(user)
+
+    for u in userList:
+        print(u.get_productName(), u.get_ID(), u.get_price(), u.get_quantity())
+        print(u.computeTotalProduct())
+
+
+    return render_template("Cart.html", usersList=usersList, count=len(usersList))
 
 
 @app.route('/login_validation', methods=['POST'])
